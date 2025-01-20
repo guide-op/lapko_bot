90360833315f3ad84e41fbe6d2afb30c78c6c774
guide-op <vobru@ephyros.com>
2025-01-20 16:54:37 +0200

    Add bot boilerplate

---
+++ main.py
@@ -0,0 +1,33 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
+
+
+def main() -> None:
+    load_dotenv()
+
+    application = ApplicationBuilder().token(os.getenv("TOKEN")).build()
+
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
