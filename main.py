b1d1e1b7d84d63eb540d884fd07673f18cafb059
guide-op <vobru@ephyros.com>
2025-01-20 23:09:04 +0200

    Add more boilerplate

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import os
 
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
 
 # Enable logging
 logging.basicConfig(
@@ -18,13 +24,46 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text
+    )
+
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = " ".join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+async def send_test_txt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_document(
+        chat_id=update.effective_chat.id,
+        document='content/mytext.txt',
+    )
+
+
+async def unknown_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Sorry, I didn't understand that command.",
+    )
+
+
 def main() -> None:
     load_dotenv()
 
     application = ApplicationBuilder().token(os.getenv("TOKEN")).build()
 
-    start_handler = CommandHandler("start", start)
-    application.add_handler(start_handler)
+    handlers = [
+        CommandHandler("start", start),
+        CommandHandler("caps", caps),
+        # CommandHandler("testfile", send_test_txt),
+        MessageHandler(filters.TEXT & (~filters.COMMAND), echo),
+        MessageHandler(filters.COMMAND, unknown_cmd),
+    ]
+
+    for handler in handlers:
+        application.add_handler(handler)
 
     application.run_polling()
 
