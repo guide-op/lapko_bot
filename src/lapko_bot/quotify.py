87cc1e6cbb86b435a98b996ba152998412561773
guide-op <vobru@ephyros.com>
2025-01-21 15:27:42 +0200

    Add Pytest coverage

---
+++ src/lapko_bot/quotify.py
@@ -0,0 +1,52 @@
+import re
+
+
+def quotify(text: str) -> str:
+    # Sub quote sequences at word and punctuation boundaries
+    text = re.sub(r'"+(?=[\w])', lambda t: "«" * len(t.group(0)), text)
+    text = re.sub(r'(?<=[\w?!.])"+', lambda t: "»" * len(t.group(0)), text)
+
+    # Sub nested quotes
+    quote_nesting_level = 0
+    text_by_char = list(text)
+    for i in range(len(text)):
+        if text[i] == "«":
+            quote_nesting_level += 1
+            if quote_nesting_level > 1:
+                text_by_char[i] = "“"
+        elif text[i] == "»":
+            if quote_nesting_level > 1:
+                text_by_char[i] = "”"
+            quote_nesting_level -= 1
+
+    return "".join(text_by_char)
+
+
+def emdashify(text: str) -> str:
+    # Separate hyphens and en dashes that are glued to words from one side
+    text = re.sub(r"(?<=\w)(--?-?|–)(?=\W)", r" \1", text)
+    text = re.sub(r"(?<=\W)(--?-?|–)(?=\w)", r"\1 ", text)
+    text = re.sub(r"^(--?-?|–)(?=\w)", r"\1 ", text)
+
+    # Convert free-floating hyphens and en dashes
+    text = re.sub(r"(?<=\W)(--?-?|–)(?=\W)", "—", text)
+    text = re.sub(r"^(--?-?|–)", "—", text)
+
+    return text
+
+
+def compress_spaces(text: str) -> str:
+    # Compress repeated spaces
+    text = re.sub(r" +", " ", text)
+
+    # Compress spaces around punctuation
+    text = re.sub(r" (?=[,.:;!?])", "", text)
+
+    return text
+
+
+def fix(text: str) -> str:
+    text = quotify(text)
+    text = emdashify(text)
+    text = compress_spaces(text)
+    return text
---
+++ tests/test_quotify.py
@@ -0,0 +1,64 @@
+# import pytest
+
+from src.lapko_bot.quotify import fix
+
+test_data_ab = []
+with open("tests/quotify_test_data_ab.txt", "r", encoding="utf8") as f:
+    # Mode = 0 - input, mode = 1 - output
+    mode = 0
+    io_pair = []
+    entry = []
+    for line in f:
+        if line.strip() == ">>>":
+            # Push input
+            io_pair.append("\n".join(entry).strip())
+            entry = []
+            mode = 1
+            continue
+        if line.strip() == "===":
+            # Push output, push in/out pair
+            io_pair.append("\n".join(entry).strip())
+            test_data_ab.append(io_pair)
+            entry = []
+            io_pair = []
+            mode = 0
+            continue
+        entry.append(line.strip())
+
+
+def test_quotify_001():
+    a, b = test_data_ab[0]
+    assert fix(a) == b
+
+
+def test_quotify_002():
+    a, b = test_data_ab[1]
+    assert fix(a) == b
+
+
+def test_quotify_003():
+    a, b = test_data_ab[2]
+    assert fix(a) == b
+
+
+# def test_quotify_with_empty_string():
+#     assert quotify("") == '""'
+
+
+# def test_quotify_with_special_characters():
+#     assert quotify("!@#$%^&*()") == '"!@#$%^&*()"'
+
+
+# def test_quotify_with_none():
+#     with pytest.raises(TypeError):
+#         quotify(None)
+
+
+# def test_quotify_with_list():
+#     with pytest.raises(TypeError):
+#         quotify([1, 2, 3])
+
+
+# def test_quotify_with_dict():
+#     with pytest.raises(TypeError):
+#         quotify({"key": "value"})
