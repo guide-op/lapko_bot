6850116f3fc3f9542a582928e8402c27778fb130
guide-op <vobru@ephyros.com>
2025-01-22 12:34:15 +0200

    Add and update string tests

--- tests/test_string_processors.py
+++ tests/test_string_processors.py
@@ -1,64 +1,30 @@
-# import pytest
+import pytest
+from pathlib import Path
 
-from lapko_bot.string_processors import fix
+from src.lapko_bot.string_processors import fix
 
-test_data_ab = []
-with open("tests/string_test_data.txt", "r", encoding="utf8") as f:
-    # Mode = 0 - input, mode = 1 - output
-    mode = 0
-    io_pair = []
-    entry = []
-    for line in f:
-        if line.strip() == ">>>":
-            # Push input
-            io_pair.append("\n".join(entry).strip())
-            entry = []
-            mode = 1
-            continue
-        if line.strip() == "===":
-            # Push output, push in/out pair
-            io_pair.append("\n".join(entry).strip())
-            test_data_ab.append(io_pair)
-            entry = []
-            io_pair = []
-            mode = 0
-            continue
-        entry.append(line.strip())
+PATH_DIR_STRING_TEST_DATA = Path("tests/string_test_data")
 
 
-def test_quotify_001():
-    a, b = test_data_ab[0]
-    assert fix(a) == b
+def load_string_test_file(filename):
+    with open(PATH_DIR_STRING_TEST_DATA / filename, "r", encoding="utf-8") as f:
+        return f.read()
 
 
-def test_quotify_002():
-    a, b = test_data_ab[1]
-    assert fix(a) == b
+# Test cases (mapping input files to expected output files)
+test_cases = [
+    ("input_dash_1_1.txt", "output_dash_1_1.txt"),
+    ("input_dash_a_1.txt", "output_dash_a_1.txt"),
+    ("input_dash_a_a.txt", "output_dash_a_a.txt"),
+    ("input_dash_direct.txt", "output_dash_direct.txt"),
+    ("input_dash_i_i.txt", "output_dash_i_i.txt"),
+    ("input_freeform.txt", "output_freeform.txt"),
+    ("input_post.txt", "output_post.txt"),
+]
 
 
-def test_quotify_003():
-    a, b = test_data_ab[2]
-    assert fix(a) == b
-
-
-def test_quotify_with_empty_string():
-    assert fix("") == ""
-
-
-# def test_quotify_with_special_characters():
-#     assert quotify("!@#$%^&*()") == '"!@#$%^&*()"'
-
-
-# def test_quotify_with_none():
-#     with pytest.raises(TypeError):
-#         quotify(None)
-
-
-# def test_quotify_with_list():
-#     with pytest.raises(TypeError):
-#         quotify([1, 2, 3])
-
-
-# def test_quotify_with_dict():
-#     with pytest.raises(TypeError):
-#         quotify({"key": "value"})
+@pytest.mark.parametrize("input_path, expected_path", test_cases)
+def test_string_converter(input_path, expected_path):
+    input_str = load_string_test_file(input_path)
+    expected_output = load_string_test_file(expected_path)
+    assert fix(input_str) == expected_output
